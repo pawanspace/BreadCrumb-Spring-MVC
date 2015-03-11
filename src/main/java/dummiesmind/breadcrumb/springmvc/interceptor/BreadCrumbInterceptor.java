f015bcb79453485ac1a645fd7841ca2064ad24b7
drndos <drndos@users.noreply.github.com>
2015-03-11 12:18:23 +0100

    Update BreadCrumbInterceptor.java

--- src/main/java/dummiesmind/breadcrumb/springmvc/interceptor/BreadCrumbInterceptor.java
+++ src/main/java/dummiesmind/breadcrumb/springmvc/interceptor/BreadCrumbInterceptor.java
@@ -85,7 +85,7 @@ public class BreadCrumbInterceptor extends HandlerInterceptorAdapter {
 			breadCrumbLink = breadCrumbObject;
 		}else{
 			breadCrumbLink = new BreadCrumbLink(link.family(), link.label(), true, link.parent());
-			String fullURL = request.getRequestURL().append("?").append(request.getQueryString()).toString();
+			String fullURL = request.getRequestURL().append((request.getQueryString()==null)?"":"?"+request.getQueryString()).toString();
 			breadCrumbLink.setUrl(fullURL);
 			createRelationships(familyMap, breadCrumbLink);
 			familyMap.put(link.label(), breadCrumbLink);
