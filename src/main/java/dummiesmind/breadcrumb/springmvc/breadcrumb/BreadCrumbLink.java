c79a35f8643b0e999603c3c35737008c9100a132
ky-lian <westnick13th@mail.ru>
2016-05-20 10:45:31 +0800

    Added Serializable interface to the BreadCrumbLink class.

--- src/main/java/dummiesmind/breadcrumb/springmvc/breadcrumb/BreadCrumbLink.java
+++ src/main/java/dummiesmind/breadcrumb/springmvc/breadcrumb/BreadCrumbLink.java
@@ -1,9 +1,12 @@
 package dummiesmind.breadcrumb.springmvc.breadcrumb;
 
+import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
 
-public class BreadCrumbLink {
+public class BreadCrumbLink implements Serializable {
+
+  private static final long serialVersionUID = -1734182996388561350L;
 
 	private BreadCrumbLink previous;
 	private List<BreadCrumbLink> next = new LinkedList<BreadCrumbLink>();
